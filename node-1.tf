88ba1be572891072db161e725a200c88d2bf5a79
jackmford <jackmitchellfordyce@gmail.com>
2025-01-11 12:53:13 -0600

    tf initial

---
+++ node-1.tf
@@ -0,0 +1,26 @@
+resource "digitalocean_droplet" "node-1" {
+  image  = "ubuntu-20-04-x64"
+  name   = "node-1"
+  region = "nyc1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.mac-air-public.id
+  ]
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = file(var.pvt_key)
+    timeout     = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # update
+      "sudo apt update",
+      "sudo apt upgrade",
+    ]
+  }
+}
