601f625fd6de5bfe08e059fa79604bd480517dc1
jackmford <jackmitchellfordyce@gmail.com>
2025-06-06 19:17:02 -0500

    dailyvim domain

--- domain.tf
+++ domain.tf
@@ -14,3 +14,8 @@ resource "digitalocean_domain" "vimtricks" {
    name = "vimtricks.jackmitchellfordyce.com"
    ip_address = digitalocean_droplet.node-1.ipv4_address
 }
+
+resource "digitalocean_domain" "dailyvim" {
+   name = "dailyvim.jackmitchellfordyce.com"
+   ip_address = digitalocean_droplet.node-1.ipv4_address
+}
